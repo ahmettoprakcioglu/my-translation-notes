6cdd66cd00c03eff022871d70005739d5251269d
Ahmet Kaan Toprakçıoğlu <56438853+ahmettoprakcioglu@users.noreply.github.com>
2024-07-22 23:48:15 +0300

    removed unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <p>app</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
